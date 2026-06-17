79d2bb495829df75e40dcd39e3fc3e62656acb6f
michalhagos <mhayele3@gmail.com>
2026-06-16 22:29:16 -0500

    Execute a query to select all products and display each product name to the screen

--- src/main/java/com/pluralsight/NorthwindTradersApp.java
+++ src/main/java/com/pluralsight/NorthwindTradersApp.java
@@ -2,7 +2,9 @@ package com.pluralsight;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 public class NorthwindTradersApp {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/northwind";
@@ -11,10 +13,14 @@ public class NorthwindTradersApp {
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected!");
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM products");
+            while (results.next()) {
+                System.out.println(results.getString("ProductName"));
+            }
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 }
- 
